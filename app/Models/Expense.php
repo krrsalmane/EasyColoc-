6d96f24794f46d4f17eb9e61b822669f241e78f2
krrsalmane <karroumsalmane@gmail.com>
2026-02-26 22:44:31 +0000

    create expense model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    protected $fillable = ['colocation_id', 'user_id', 'category_id', 'title', 'amount', 'date'];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- database/migrations/2026_02_26_224116_create_expenses_table.php
+++ database/migrations/2026_02_26_224116_create_expenses_table.php
@@ -11,10 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('expenses', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+       Schema::create('expenses', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('colocation_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+        $table->string('title');
+        $table->decimal('amount', 10, 2);
+        $table->date('date');
+        $table->timestamps();
+    });
     }
 
     /**
